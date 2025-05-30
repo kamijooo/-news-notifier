cb4633fd92ea8c4e6ee25bfcc0406e6ef5992da8
kamijooo <kamisayu426@gmail.com>
2025-05-30 20:37:10 +0900

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -1,3 +1,14 @@
+import axios from "axios";
+
+const SLACK_WEBHOOK_URL = "https://hooks.slack.com/services/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";
+
 const payload = {
-  text: "✅ Webhook通知テスト（" + new Date().toLocaleString() + "）"
+  text: "✅ 通知テスト（最小構成）"
 };
+
+try {
+  await axios.post(SLACK_WEBHOOK_URL, payload);
+  console.log("✅ Slackテスト通知送信成功");
+} catch (err) {
+  console.error("❌ Slack送信失敗:", err.message);
+}
